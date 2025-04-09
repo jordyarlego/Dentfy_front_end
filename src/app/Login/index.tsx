420b2b86cadde5580e5f3377ae265f01b310590b
jordyarlego <jordyarlego@gmail.com>
2025-04-08 23:47:12 -0300

    First Commit :fire:

---
+++ src/app/Login/index.tsx
@@ -0,0 +1,10 @@
+import InputTeste from '../../components/ComponentInput'
+
+export default function Login(){
+    return(
+        <div>
+            <h1>Login</h1>
+            <InputTeste/>
+        </div>
+    )
+}
---
+++ src/components/ComponentInput/index.tsx
@@ -0,0 +1,12 @@
+export default function InputTeste() {
+    return (
+      <div className="w-full">
+        <input
+          type="text"
+          className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Digite algo..."
+        />
+      </div>
+    );
+  }
+  
